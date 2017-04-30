5f25a32935c32547b3df897417313660870af6b3
StephenPatrick <sythe2o0@gmail.com>
2017-04-30 18:05:52 -0500

    Establish consistent seeds, measure creation vs update time on benchmarks

--- dcel/pointLoc/test/point-loc_test.go
+++ dcel/pointLoc/test/point-loc_test.go
@@ -1,8 +1,10 @@
 package test
 
 import (
+	"fmt"
 	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/200sc/go-compgeo/dcel"
 	"github.com/200sc/go-compgeo/dcel/pointLoc"
@@ -16,9 +18,14 @@ import (
 )
 
 var (
-	inputSize  = 100
-	inputRange = 10000.0
-	testCt     = 1000
+	inputSize   = 3
+	inputRange  = 10000.0
+	testCt      = 10000
+	slabErrors  = 0
+	trapErrors  = 0
+	rtreeErrors = 0
+	plumbErrors = 0
+	seed        int64
 )
 
 func randomPt() geom.D3 {
@@ -26,13 +33,15 @@ func randomPt() geom.D3 {
 		rand.Float64()*inputRange, 0)
 }
 
-func testRandomPts(t *testing.T, pl pointLoc.LocatesPoints, limit int) {
+func testRandomPts(t *testing.T, pl pointLoc.LocatesPoints, limit int, errs *int) {
 	for i := 0; i < limit; i++ {
 		pt := randomPt()
 		structIntersected, err := pl.PointLocate(pt.X(), pt.Y())
 		bruteForceContains := structIntersected.Contains(pt)
 		assert.Nil(t, err)
-		assert.True(t, bruteForceContains)
+		if !assert.True(t, bruteForceContains) {
+			(*errs)++
+		}
 	}
 }
 
@@ -42,7 +51,8 @@ func TestRandomDCELSlab(t *testing.T) {
 	structure, err := slab.Decompose(dc, tree.RedBlack)
 	assert.Nil(t, err)
 
-	testRandomPts(t, structure, testCt)
+	testRandomPts(t, structure, testCt, &slabErrors)
+	printErrors()
 }
 
 func TestRandomDCELTrapezoid(t *testing.T) {
@@ -51,7 +61,8 @@ func TestRandomDCELTrapezoid(t *testing.T) {
 	_, _, structure, err := trapezoid.TrapezoidalMap(dc)
 	assert.Nil(t, err)
 
-	testRandomPts(t, structure, testCt)
+	testRandomPts(t, structure, testCt, &trapErrors)
+	printErrors()
 }
 
 func TestRandomDCELPlumbLine(t *testing.T) {
@@ -59,7 +70,8 @@ func TestRandomDCELPlumbLine(t *testing.T) {
 
 	it := bruteForce.PlumbLine(dc)
 
-	testRandomPts(t, it, testCt)
+	testRandomPts(t, it, testCt, &plumbErrors)
+	printErrors()
 }
 
 func TestRandomDCELRtree(t *testing.T) {
@@ -67,14 +79,31 @@ func TestRandomDCELRtree(t *testing.T) {
 
 	pl := rtree.DCELtoRtree(dc)
 
-	testRandomPts(t, pl, testCt)
+	testRandomPts(t, pl, testCt, &rtreeErrors)
+	printErrors()
+}
+
+func printErrors() {
+	fmt.Println("Total errors")
+	fmt.Println("Slab:", slabErrors)
+	fmt.Println("Trapezoid:", trapErrors)
+	fmt.Println("Plumb Line:", plumbErrors, "(Should be zero)")
+	fmt.Println("Rtree:", rtreeErrors)
+	fmt.Println()
 }
 
 func BenchmarkRandomDCELSlab(b *testing.B) {
+	if seed == 0 {
+		fmt.Println("Setting seed")
+		seed = time.Now().UnixNano()
+	}
+	rand.Seed(seed)
 	dc := dcel.Random2DDCEL(inputRange, inputSize)
 
 	pl, _ := slab.Decompose(dc, tree.RedBlack)
 
+	rand.Seed(seed)
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		pt := randomPt()
 		pl.PointLocate(pt.X(), pt.Y())
@@ -82,10 +111,20 @@ func BenchmarkRandomDCELSlab(b *testing.B) {
 }
 
 func BenchmarkRandomDCELTrapezoid(b *testing.B) {
+	// This seed pattern guarantees that
+	// each benchmark is run with the same
+	// input dcel and input points
+	if seed == 0 {
+		fmt.Println("Setting seed")
+		seed = time.Now().UnixNano()
+	}
+	rand.Seed(seed)
 	dc := dcel.Random2DDCEL(inputRange, inputSize)
 
 	_, _, pl, _ := trapezoid.TrapezoidalMap(dc)
 
+	rand.Seed(seed)
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		pt := randomPt()
 		pl.PointLocate(pt.X(), pt.Y())
@@ -93,10 +132,17 @@ func BenchmarkRandomDCELTrapezoid(b *testing.B) {
 }
 
 func BenchmarkRandomDCELRtree(b *testing.B) {
+	if seed == 0 {
+		fmt.Println("Setting seed")
+		seed = time.Now().UnixNano()
+	}
+	rand.Seed(seed)
 	dc := dcel.Random2DDCEL(inputRange, inputSize)
 
 	pl := rtree.DCELtoRtree(dc)
 
+	rand.Seed(seed)
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		pt := randomPt()
 		pl.PointLocate(pt.X(), pt.Y())
@@ -104,12 +150,67 @@ func BenchmarkRandomDCELRtree(b *testing.B) {
 }
 
 func BenchmarkRandomDCELPlumbLine(b *testing.B) {
+	if seed == 0 {
+		fmt.Println("Setting seed")
+		seed = time.Now().UnixNano()
+	}
+	rand.Seed(seed)
 	dc := dcel.Random2DDCEL(inputRange, inputSize)
 
 	pl := bruteForce.PlumbLine(dc)
 
+	rand.Seed(seed)
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		pt := randomPt()
 		pl.PointLocate(pt.X(), pt.Y())
+	}
+}
+
+func BenchmarkRandomSetupSlab(b *testing.B) {
+	if seed == 0 {
+		fmt.Println("Setting seed")
+		seed = time.Now().UnixNano()
+	}
+	rand.Seed(seed)
+	for i := 0; i < b.N; i++ {
+		dc := dcel.Random2DDCEL(inputRange, inputSize)
+		slab.Decompose(dc, tree.RedBlack)
+	}
+}
+
+func BenchmarkRandomSetupTrapezoid(b *testing.B) {
+	if seed == 0 {
+		fmt.Println("Setting seed")
+		seed = time.Now().UnixNano()
+	}
+	rand.Seed(seed)
+	for i := 0; i < b.N; i++ {
+		dc := dcel.Random2DDCEL(inputRange, inputSize)
+		trapezoid.TrapezoidalMap(dc)
+	}
+}
+
+func BenchmarkRandomSetupRtree(b *testing.B) {
+	if seed == 0 {
+		fmt.Println("Setting seed")
+		seed = time.Now().UnixNano()
+	}
+	rand.Seed(seed)
+	for i := 0; i < b.N; i++ {
+		dc := dcel.Random2DDCEL(inputRange, inputSize)
+		rtree.DCELtoRtree(dc)
+	}
+}
+
+func BenchmarkRandomSetupPlumbLine(b *testing.B) {
+	if seed == 0 {
+		fmt.Println("Setting seed")
+		seed = time.Now().UnixNano()
+	}
+	rand.Seed(seed)
+	for i := 0; i < b.N; i++ {
+		dc := dcel.Random2DDCEL(inputRange, inputSize)
+		bruteForce.PlumbLine(dc)
 	}
 }
